f09745afad90d25c2dcfb6c9900520a6a08f1103
Vyacheslav <goodgamefnaf@gmail.com>
2024-11-04 11:53:38 +0200

    homewoke12

---
+++ script.js
@@ -0,0 +1,41 @@
+//Homework 12.1
+let link;
+function getLink() {
+  let linkValue = prompt(`enter a link`);
+
+  return (link = linkValue);
+}
+function goToLink() {
+  location.href = link;
+}
+
+//Homework 12.2
+
+const sectorButton = document.querySelector(`#buttonMain`);
+sectorButton.addEventListener(`click`, function (e) {
+  e.defaultPrevented;
+  console.log(e.target.textContent);
+});
+
+//Homework 12.3
+
+const addButton = document.querySelector(`#addButton`);
+const inputTask = document.querySelector(`#inputTask`);
+const tasksTree = document.querySelector(`#tasksTree`);
+addButton.addEventListener(`click`, function (e) {
+  const newTask = document.createElement(`li`);
+  const newTaskText = document.createElement(`p`);
+  const newTaskButton = document.createElement("button");
+  newTaskButton.setAttribute(`type`, `button`);
+  newTaskButton.setAttribute(`onclick`, `removeItem(event)`);
+  newTaskButton.innerText = `Видалити`;
+  newTaskText.innerText = inputTask.value;
+  tasksTree.appendChild(newTask);
+  newTask.appendChild(newTaskText);
+  newTask.appendChild(newTaskButton);
+});
+function removeItem(event) {
+  const currentLi = event.target;
+  let mainLi = currentLi.closest(`li`);
+  mainLi.remove();
+}
