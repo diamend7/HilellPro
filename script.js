fb487cdcd86d6aa6b48d9a6fa22484a9cfbd274d
diamend7 <goodgamefnaf@gmail.com>
2024-11-06 18:13:58 +0200

    homework13_2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function validateForm(event){
+const submit = document.querySelector(`#submit`);
+submit.addEventListener(`click`, function(event){
     event.preventDefault();
+
+    let isValid = true
 
     const name = document.querySelector(`#name`).value.trim();
     const message = document.querySelector(`#message`).value.trim();
@@ -11,37 +14,41 @@ function validateForm(event){
     const phoneError = document.querySelector(`#phoneError`);
     const emailError = document.querySelector(`#emailError`);
 
-    nameError.textContent = ``;
-    messageError.textContent = ``;
-    phoneError.textContent = ``;
-    emailError.textContent = ``;
-
     if(name === `` || !isNaN(name)){
         nameError.textContent = `NOO this is wrong!`;
-    } else {
-        console.log(name)
-    }
+        isValid = false
+    } 
 
     if(message.length < 5){
         messageError.textContent = `NOO type more!`;
-    } else {
-        console.log(message)
-    }
+        isValid = false
+    } 
 
     const phoneRegEx = /^380\d{9}$/;
     // без плюсу тому що поведінка type=phone виводить у консоль без плюсу навіть коли ти його пишеш
     if(!phoneRegEx.test(phone)){
         phoneError.textContent = `NOO wrong number!`;
-    } else {
-        console.log(`+` + phone)
-    }
+        isValid = false
+    } 
 
 
     const emailRegEx = /^[^\s@]+@{1}[^\s@]+\.{1}[^\s@]+$/;
     if(!emailRegEx.test(email)){
         emailError.textContent = `NOO wrong email!`;
-    } else {
-        console.log(email)
+        isValid = false
+    } 
+
+
+    if(isValid === true){
+        console.log(name);
+        console.log(message);
+        console.log(`+${phone}`);
+        console.log(email);
     }
  
-}
+});
+
+function nameErrorFunc(){ nameError.textContent = ``};
+function messageErrorFunc(){ messageError.textContent = ``};
+function phoneErrorFunc(){ phoneError.textContent = ``};
+function emailErrorFunc(){ emailError.textContent = ``};
